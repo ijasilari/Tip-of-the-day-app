e4e6fc97d518bf92d330609e867c9f332559621d
Daniil Kovalev <daniilinfinland@gmail.com>
2023-05-03 12:53:04 +0300

    init_db styling update

--- database/init_db.sql
+++ database/init_db.sql
@@ -4,11 +4,11 @@ CREATE TABLE public.tips (
 );
 
 CREATE TABLE users (
-    id VARCHAR(36) NOT NULL,
-    username VARCHAR(100) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `id` VARCHAR(36) NOT NULL,
+    `username` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(60) NOT NULL,
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 )
