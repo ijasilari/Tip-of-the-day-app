1faf30e7e8d0b1855ea7bb1c7ea037f4f1e25f04
Ilari <ijasilari@gmail.com>
2023-05-08 14:20:49 +0300

    intervalCard categoryTitle

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -6,30 +6,60 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 const Card = () => {
   const [cardData, setCardData] = useState(null);
+  const [cardCategory, setCardCategory] = useState("");
+  const [dataLoaded, setDataLoaded] = useState(false);
 
+
+  const categoryOptions = [
+    {value: 0, label: "Other"},
+    {value: 1, label: "CSS"},
+    {value: 2, label: "Java"},
+    {value: 3, label: "JavaScript"},
+    {value: 4, label: "HTTP"},
+    {value: 5, label: "Python"},
+    {value: 6, label: "CPP"},
+    {value: 7, label: "Dart"},
+    {value: 8, label: "Flutter"},
+    {value: 9, label: "Rust"},
+    {value: 10, label: "Linux"}
+  ]
+
+  let responseCategoryNum = 0;
+  let categoryLabel = "";
   const fetchCardData = async () => {
+
     const response = await axios.get(`${process.env.REACT_APP_LOCAL_BACKEND_URL}/randomtip`);
     setCardData(response.data.tip);
-    console.log(response);
+    setCardCategory(response.data.tip.category);
+    //console.log(response);
+
+    responseCategoryNum = response.data.tip.category
+    categoryLabel = categoryOptions[responseCategoryNum]
+    //console.log(k)
+    setCardCategory(categoryLabel.label);
   };
 
   useEffect(() => {
     fetchCardData();
+    setDataLoaded(true);
   }, [])
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      fetchCardData();
-    }, 15000);
-  
-    return () => clearInterval(interval);
-  }, []);
+    if(dataLoaded){
+
+      const interval = setInterval(() => {
+        fetchCardData();
+      }, 12000);
+      return () => clearInterval(interval);
+    }
+    
+  }, [dataLoaded]);
 
   return (
     <div className="card-container">
       {cardData ? (
         <div className="card">
-          <h2 className="title">{cardData.id}</h2>
+          <h2 className="title">{cardCategory}</h2>
           <ReactMarkdown
                     children={cardData.description}
                     components={{
