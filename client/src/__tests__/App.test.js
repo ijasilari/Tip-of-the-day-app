88e612442847ce379ec16d567efd2a82dd86609d
Ilari <ijasilari@gmail.com>
2023-05-09 15:50:58 +0300

    testit alku

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import HomePage from './pages/HomePage';
+import HomePage from '../pages/HomePage';
 
 test('renders learn react link', () => {
   render(<HomePage />);
