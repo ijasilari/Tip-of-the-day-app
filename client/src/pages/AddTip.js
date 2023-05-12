228d4f135d799924e496c263e881aaf65727114a
Jambo258 <jambomyllymaa@gmail.com>
2023-05-12 10:48:40 +0300

    updated error handling for new tip category

--- client/src/pages/AddTip.js
+++ client/src/pages/AddTip.js
@@ -79,7 +79,9 @@ function AddTip() {
         console.log(response);
         formikTip.values.description = "";
         formikTip.errors.description = "";
-      } catch (err) {}
+      } catch (err) {
+        console.log(err.message)
+      }
     };
 
 
@@ -90,12 +92,16 @@ function AddTip() {
       if (!values.description) {
         errors.description = "Required tip description";
       }
+      if(!values.category){
+        errors.category = "Required tip category"
+      }
       return errors;
     };
 
     const formikTip = useFormik({
       initialValues: {
         description: "",
+        category: category,
       },
       validate: validateTip,
       onSubmit: AddNewTip,
@@ -112,12 +118,6 @@ function AddTip() {
       >
         Add New Tip To The List
       </Typography>
-      <Dropdown
-          isSearchable
-          placeHolder="Select..."
-          options={categoryOptions}
-          onChange={(value) => setCategory(value.value)}
-        />
       <Box
         component="form"
         onSubmit={formikTip.handleSubmit}
@@ -129,6 +129,25 @@ function AddTip() {
         noValidate
         autoComplete="off"
       >
+        <Box
+          flexGrow="1"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            "& > :not(style)": { mt: 3, ml: "125%", mr: "31%", width: "100%" },
+          }}
+        >
+          <Dropdown
+            isSearchable
+            placeHolder="Select..."
+            options={categoryOptions}
+            onChange={(value) => {
+              setCategory(value.value);
+              formikTip.values.category = value.value;
+            }}
+          />
+        </Box>
         <TextField
           id="description"
           name="description"
@@ -140,6 +159,14 @@ function AddTip() {
           onChange={formikTip.handleChange}
           value={formikTip.values.description}
         />
+        {formikTip.errors.category ? (
+          <Box
+            display="inline-flex"
+            style={{ color: "red", textAlign: "inherit" }}
+          >
+            {formikTip.errors.category}
+          </Box>
+        ) : null}
         {formikTip.errors.description ? (
           <Box
             display="inline-flex"
@@ -156,38 +183,46 @@ function AddTip() {
         >
           Add New Tip
         </Button>
-        <Container style={{ background: '#f2f6fc', display: 'block', marginLeft: 'auto', marginRight: 'auto'}}>
+        <Container
+          style={{
+            background: "#f2f6fc",
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
           <h1>Attention!</h1>
-          <div style={{textAlign: 'left'}}>
-            <p>This page supports markdown and syntax highlight code.
-               To create codeblock with highlight write:</p>
-            <ReactMarkdown children={codeBlSyntax}/>
+          <div style={{ textAlign: "left" }}>
+            <p>
+              This page supports markdown and syntax highlight code. To create
+              codeblock with highlight write:
+            </p>
+            <ReactMarkdown children={codeBlSyntax} />
             <p>Example:</p>
-            <ReactMarkdown children={syntaxExample}/>
+            <ReactMarkdown children={syntaxExample} />
             <p>Output:</p>
             <ReactMarkdown
-                    children={codeExample}
-                    components={{
-                      code({ node, inline, className, children, ...props }) {
-                        const match = /language-(\w+)/.exec(className || "");
-                        return !inline && match ? (
-                          <SyntaxHighlighter
-                            children={String(children).replace(/\n$/, "")}
-                            language={match[1]}
-                            {...props}
-                          />
-                        ) : (
-                          <code className={className} {...props}>
-                            {children}
-                          </code>
-                        );
-                      },
-                    }}
-              />
+              children={codeExample}
+              components={{
+                code({ node, inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || "");
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      children={String(children).replace(/\n$/, "")}
+                      language={match[1]}
+                      {...props}
+                    />
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  );
+                },
+              }}
+            />
           </div>
         </Container>
       </Box>
-
     </div>
   );
 }
