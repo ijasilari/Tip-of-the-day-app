cc7a6bda4235f484e11adf38704b97023744ace5
Ilari Ijas <ilari.ijas@tuni.fi>
2023-05-17 15:59:26 +0300

    uusin theme päivitys KESKEN

--- client/src/App.js
+++ client/src/App.js
@@ -75,7 +75,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage theme={theme}/>} />
         <Route path="/profilepage" element={<ProfilePage userId={userId} theme={theme}/>} />
-        <Route path="addtip" element={<AddTip />} />
+        <Route path="addtip" element={<AddTip theme={theme}/>} />
         <Route path="viewtips" element={<ViewTips theme={theme}/>} />
         <Route path="owntips" element={<OwnTips theme={theme}/>} />
         <Route path="*" element={<HomePage />} />
